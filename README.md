c4fcc6b03f7f3e23f5fac1462fb4183303148061
piotrek-desktop <piotrrybiec28@gmail.com>
2021-02-19 23:13:32 +0100

    README array

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# sac
+# Sorting Algorithms Comparison
+
+### Array
+
+Array that all of the algorithms struggle was genereted completly randomly by the following code:
+
+```python3
+from numpy import random
+
+arr = list(random.randint(1337, size=(12)))
+
+arr 
+
+In [1] [1160, 563, 916, 736, 658, 81, 1104, 302, 787, 58, 1220, 1149]
+
+```
+
---
+++ src/mergesort.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+def mergesort(a):
+  if len(a) == 1:
+    return a
+  elif len(a) == 2:
+    if a[0] > a[1]:
+      return [a[1], a[0]]
+    else:
+      return a 
+
+  piv = len(a)//2
+  m1 = mergesort(a[:piv])
+  m2 = mergesort(a[piv:])
+
+  acc = []
+  while True:
+    if len(m1) > 0 and len(m2) > 0:
+      if m1[0] > m2[0]:
+        acc.append(m2[0])
+        m2 = m2[1:]
+      else:
+	    acc.append(m1[0])
+        m1 = m1[1:]
+    elif len(m1) > 0:
+      acc.append(m1[0])
+      m1 = m1[1:]
+    elif len(m2) > 0:
+      acc.append(m2[0])
+      m2 = m2[1:]
+    else:
+      break
+  return acc
+					
+if __name__ == '__main__':        
+  arr = []
+  print(mergesort(arr))
