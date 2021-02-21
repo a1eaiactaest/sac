cfd91c2a7cb72ed28b7e66a82d7fb5a02ee5305a
piotrek@manjaro <piotrrybiec28@gmail.com>
2021-02-21 21:58:08 +0100

    unittests

--- src/quicksort.py
+++ src/quicksort.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import fileinput
 
 def partition(a, l, h): # array, low, high
   piv = a[h] # set pivoot
@@ -18,8 +19,12 @@ def quicksort(a, l, h):
     piv_loc = partition(a, l, h) # locate pivot
     quicksort(a, l, piv_loc-1)
     quicksort(a, piv_loc+1, h)
+  return a
 
-arr = [1160, 563, 916, 736, 658, 81, 1104, 302, 787, 58, 1220, 1149]
-quicksort(arr, 0, len(arr)-1)
-print(arr)
+def main():
+  arr = [int(line) for line in fileinput.input(files='input.txt')]
+  print(quicksort(arr, 0, len(arr)-1))
+
+if __name__ == '__main__':
+  main()
 
