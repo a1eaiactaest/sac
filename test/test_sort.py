c6d8007346c83ca773256c20f4e8fe4d7610ad39
piotrek@manjaro <piotrrybiec28@gmail.com>
2021-02-21 22:03:31 +0100

    didnt add files

--- test/test_sort.py
+++ test/test_sort.py
@@ -5,10 +5,22 @@ import sys
 sys.path.insert(0, '../src')
 from mergesort import mergesort
 from quicksort import quicksort
+from insertionsort import insertionsort
 
 #arr = open('../src/input.txt', 'r').read().split('\n')
 #arr = [x for x in arr if x]
-arr = [x for x in open('../src/input.txt', 'r').read().split('\n') if x]
-my_sorted = [x for x in open('../src/sorted.txt', 'r').read().split('\n') if x]
+arr = [int(x) for x in open('../src/input.txt', 'r').read().split('\n') if x]
+my_sorted = [int(x) for x in open('../src/sorted.txt', 'r').read().split('\n') if x]
 
-class 
+class TestSort(unittest.TestCase):
+  def test_mergesort(self):
+    self.assertEqual(mergesort(arr), my_sorted, None)
+
+  def test_quicksort(self):
+    self.assertEqual(quicksort(arr, 0, len(arr)-1), my_sorted, None)
+
+  def test_insertionsort(self):
+    self.assertEqual(insertionsort(arr), my_sorted, None)
+
+if __name__ == '__main__':
+  unittest.main()
